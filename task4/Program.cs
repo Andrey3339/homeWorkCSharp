d5d5033a06376448c457dea344c68901c08870a5
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-01-29 23:11:37 +0500

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите любое целое положительное число: ");
+int a = int.Parse(Console.ReadLine());
+int i = 1;
+Console.Write("Все четные числа меньше введеного: ");
+while(i <= a)
+{
+    if(i % 2 == 0)
+        Console.Write(i + " ");
+    i++;
+}
+
