a67d4f715e47d30a16c2d344089bdf82f4293718
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-01-29 23:13:35 +0500

    add ! in task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Clear();
 Console.Write("Введите любое целое положительное число: ");
-int a = int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine()!);
 int i = 1;
 Console.Write("Все четные числа меньше введеного: ");
 while(i <= a)
