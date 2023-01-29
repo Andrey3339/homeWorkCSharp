aa6a2ac31ef29b1cf43ac884d199e9d090f40d9a
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-01-29 23:01:54 +0500

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите любое целое число: ");
+long a = long.Parse(Console.ReadLine())!;
+if ( a % 2 == 0)
+    Console.WriteLine($"Число {a} четное.");
+else
+    Console.WriteLine($"Число {a} нечетное.");
+
