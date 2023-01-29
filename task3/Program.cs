383a4c8d7e346a742361e6d6fe5f2ef96edd4ff0
Andrey3339 <120047057+Andrey3339@users.noreply.github.com>
2023-01-29 23:53:35 +0500

    Delete task3 directory

--- task3/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.Clear();
-Console.Write("Введите любое целое число: ");
-long a = long.Parse(Console.ReadLine()!);
-if ( a % 2 == 0)
-    Console.WriteLine($"Число {a} четное.");
-else
-    Console.WriteLine($"Число {a} нечетное.");
-
