8a09e0ba0b4576d7c6def2d5590c2a3bcffa3b00
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-01-29 23:24:11 +0500

    rename task1 to task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,14 +1,16 @@
 ﻿Console.Clear();
-Console.WriteLine("Введите три числа: ");
+Console.WriteLine("Введите два числа: ");
 Console.Write("Введите первое число:  ");
 int a = int.Parse(Console.ReadLine()!);
-Console.Write("Введите второе число:  ");
+Console.Write("введите второе число:  ");
 int b = int.Parse(Console.ReadLine()!);
-Console.Write("Введите третье число:  ");
-int c = int.Parse(Console.ReadLine()!);
-int max = a;
-if (b > max) 
-    max = b;
-if (c > max)
-    max = c;
-Console.WriteLine($"Максимальное число из 3х равно: {max}");
+if (a > b) 
+    {
+        Console.WriteLine($"Первое число {a} больше второго {b}.");
+    }
+else if (a < b)
+    {   
+        Console.WriteLine($"Второе число {b} больше первого {a}.");
+    }
+else
+    Console.WriteLine("Числа равны.");
